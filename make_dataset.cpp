90965e2c9f828d369f366bae7c1ea741b9dd199b
Stevan Dupor <smdupor@gmail.com>
2021-09-21 23:10:52 -0400

    Add C++ dataset builder for large graph building

--- make_dataset.cpp
+++ make_dataset.cpp
@@ -9,7 +9,7 @@ int main() {
    std::string temp;
 
    for(uint64_t i = 0; i < 0b1111111111111111111111; i++){
-      temp = std::to_string(rand() % 0xff) +  "\t"  + std::to_string(rand() % 0xff) + "\n";
+      temp = std::to_string(rand() % 0xfffff) +  "\t"  + std::to_string(rand() % 0xfffff) + "\n";
       fp << temp;
    }
    fp.close();
