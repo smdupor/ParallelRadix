5ed76e0e2fa75e30aa06dfc89978c513352810de
Stevan Dupor <smdupor@gmail.com>
2021-09-28 11:44:43 -0400

    add 30b8 mpi control

---
+++ Shellscripts/30b8mpi.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=30b8mpi      # Job name
+#SBATCH --mail-type=END,FAIL         # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=smdupor@ncsu.edu    # Where to send mail	
+#SBATCH -p skylake
+#SBATCH --nodes=2                    # Run all processes on a single node	
+#SBATCH --time=01:15:00              # Time limit hrs:min:sec
+#SBATCH --output=30b8-mpi%j.log     # Standard output and error log
+pwd; hostname; date
+
+echo "Running 30b8GB large dataset on $SLURM_CPUS_ON_NODE CPU cores"
+mpirun -n 2 -bootstrap slurm /home/smdupor/ParallelRadix/bin/main_mpi -f /mnt/beegfs/smdupor/dataset30b8.txt -n 4 -r 1659025
+
+date
+
+
